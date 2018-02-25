47a874050097a0686e33797eb28fbbefab1a0e6f
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2018-02-25 01:23:50 +0000

    Make a the cube an object
    Start making propper code instead of a hack.
    
    The cube will be an object to which functions will be added in order to provide basic rotations.

--- Rubiks.py
+++ Rubiks.py
@@ -1,15 +1,21 @@
 import pprint
 
-cube = [[[0 for k in range(3)] for j in range(3)] for i in range(6)]
-colours = ['W', 'O', 'G' , 'R', 'B', 'Y']
-color = 0
+class PyCube:
+    def __init__(self):
+        cube = [[[0 for k in range(3)] for j in range(3)] for i in range(6)]
+        colours = ['W', 'O', 'G' , 'R', 'B', 'Y']
+        color = 0
+    
+        for i in range(6):
+            for j in range(3):
+                for k in range(3):
+                    cube[i][j][k] = colours[color]
+            color += 1
+    
+    def show(self):
+        pprint.pprint(self.cube)
 
-pprint.pprint(cube)
-
-for i in range(6):
-    for j in range(3):
-        for k in range(3):
-            cube[i][j][k] = colours[color]
-    color += 1
-
-pprint.pprint(cube)
+#Just for testing purposes
+def main():
+    testCube = PyCube()
+    testCube.show()
