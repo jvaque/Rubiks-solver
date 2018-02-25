fcffec0b0ff0f3953d3a7133ec2d81c5f7aba10f
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2018-02-25 23:51:01 +0000

    Made white face rotation
    Modify the array cube so it can rotate the white face clockwise

--- Rubiks.py
+++ Rubiks.py
@@ -2,20 +2,70 @@ import pprint
 
 class PyCube:
     def __init__(self):
-        cube = [[[0 for k in range(3)] for j in range(3)] for i in range(6)]
-        colours = ['W', 'O', 'G' , 'R', 'B', 'Y']
+        self.cube = [[['' for k in range(3)] for j in range(3)] for i in range(6)]
+        self.rotatedFace = [[['' for k in range(3)] for j in range(3)], [['' for k in range(3)] for j in range(4)]]
+        colours = ['W', 'O', 'G', 'R', 'B', 'Y']
         color = 0
-    
         for i in range(6):
             for j in range(3):
                 for k in range(3):
-                    cube[i][j][k] = colours[color]
+                    self.cube[i][j][k] = colours[color]
             color += 1
-    
+        
+        #for testing
+        # counter = 10
+        # for i in range(6):
+        #     for j in range(3):
+        #         for k in range(3):
+        #             self.cube[i][j][k] = counter
+        #             counter += 1
+        #delete between coments
+
     def show(self):
+        print()
         pprint.pprint(self.cube)
+        print()
+        pprint.pprint(self.rotatedFace)
+    
+    def clearRotatedFace(self):
+        for i in range(2):
+            for j in range(3+i):
+                for k in range(3):
+                    self.rotatedFace[i][j][k] = ''
+    
+    def rotateWhite(self):
+        #Copying over face
+        for i in range(3):
+            for j in range(3):
+                self.rotatedFace[0][i][j] = self.cube[0][i][j]
+        #copying over the connected edges
+        for i in range(4):
+            for j in range(3):
+                self.rotatedFace[1][i][j] = self.cube[i+1][0][j]
+        
+        #move things
+        for i in range(3):
+            k = 2
+            for j in range(3):
+                self.cube[0][i][j] = self.rotatedFace[0][k][i]
+                k -= 1
+        
+        for i in range(4):
+            for j in range(3):
+                if i == 3:
+                    self.cube[i+1][0][j] = self.rotatedFace[1][0][j]
+                else:
+                    self.cube[i+1][0][j] = self.rotatedFace[1][i+1][j]
+        
+        self.clearRotatedFace()
+    
+    
 
 #Just for testing purposes
 def main():
     testCube = PyCube()
     testCube.show()
+    testCube.rotateWhite()
+    testCube.show()
+
+main()
